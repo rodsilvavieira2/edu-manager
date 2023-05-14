ad3aab6f1c7740e5c9530b882732a11c05e18d67
Rodrigo Silva <rodsilvavieira2@gmail.com>
2023-05-14 11:30:25 -0400

    feat: screen header component

---
+++ src/components/screen-header.tsx
@@ -0,0 +1,24 @@
+import { Box, Heading } from 'native-base'
+
+export interface ScreenHeaderProps {
+  title: string
+}
+
+export function ScreenHeader({ title }: ScreenHeaderProps) {
+  return (
+    <Box
+      safeAreaTop
+      alignItems="center"
+      justifyContent="center"
+      flexDir="row"
+      px="4"
+      py="4"
+      bg="primary.500"
+      shadow={8}
+    >
+      <Heading color="white" fontSize="lg">
+        {title}
+      </Heading>
+    </Box>
+  )
+}
